8c170afda49cdb6ee3b948e672f2b6f3653dffc5
Goldrich <ildaraltynbaev89@gmail.com>
2026-03-28 14:53:42 +0300

    debug true

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def analyze_audio():
         os.unlink(wav_path)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
